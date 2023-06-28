e0083ceb75fc65607b5d9f223814058e08bcd932
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-06-27 21:54:50 -0300

    att

--- src/pages/player/Player.js
+++ src/pages/player/Player.js
@@ -1,28 +1,39 @@
 import React from 'react'
 import Like from './like_btn/Like_btn'
 import Dislike from './dislike_btn/Dislike_btn'
+import { AiFillEye, AiFillHeart } from 'react-icons/ai'
+import pingu from '../../assets/image/pingu.mp4'
 import './Player.css'
+import Channel_component from './channel_component/Channel_component'
 
 function Player() {
   return (
     <>
-
       <div>
-
+        <video  controls className='video__player'>
+        <source src={pingu} type="video/mp4"/>
+        </video>
       </div>
-      <div>
-        <div>
-
-        </div>
-        <div>
-
+      <div className='video__title'>
+        <p>Pingu.</p>
+      </div>
+      <div className='interaction'>
+        <div className='interaction__info'>
+          <div className='views__div'>
+            <AiFillEye size={'1.3rem'} /> 57k de Visualizações
+          </div>
+          <div className='likes__div'>
+            <AiFillHeart size={'1.25rem'} /> 57k de Likes
+          </div>
         </div>
         <div className='like__dislike__btns'>
           <Like />
           <Dislike />
         </div>
       </div>
-
+      <div>
+        <Channel_component/>
+     </div>
     </>
   )
 }
---
+++ src/pages/player/channel_component/Channel_component.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Channel_component.css'
+
+function Channel_component() {
+  return (
+    <div>
+        <div className='component'>
+            <div>
+                <img src='https://yt3.ggpht.com/PFRD_rpPwAIY-FC2t6Ob0GpJe2udeEaXNwug4Dx8v7zxxda6ZKHU1aKBX-XoWvYh2H4Ow6TtBDk=s176-c-k-c0x00ffffff-no-rj-mo' className='channel__image'/>
+            </div>
+            <div>
+                <div className='channel__name'>Pingu - OfficialChannel</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Channel_component
