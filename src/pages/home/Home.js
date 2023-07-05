ec89bccd54e10c497dc86341776906f305014040
Gustavo Guilherme de Abreu <137020767+GustavoAbreu01@users.noreply.github.com>
2023-07-05 19:34:56 -0300

    ATT

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './header/Header'
+import './Home.css'
+import MySlider from './slider/Slider'
 
 function Home() {
   return (
     <><Header />
       <div className='container__home'>
-
+        <div className='container__slider__base'>
+          <MySlider />
+        </div>
       </div>
     </>
 
