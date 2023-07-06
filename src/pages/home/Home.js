123c2390f78a9d69fa72df6b4f82264685662218
Gustavo Guilherme de Abreu <137020767+GustavoAbreu01@users.noreply.github.com>
2023-07-06 20:18:05 -0300

    ATT

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './header/Header'
 import './Home.css'
 import MySlider from './slider/Slider'
+import Video_card from '../../components/video_card/Video_card'
 
 function Home() {
   return (
@@ -9,6 +10,10 @@ function Home() {
       <div className='container__home'>
         <div className='container__slider__base'>
           <MySlider />
+        </div>
+        <div className='container__video__cards'>
+          <Video_card />
+          <Video_card />
         </div>
       </div>
     </>
--- src/pages/home/header/Header.js
+++ src/pages/home/header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { FiUpload } from 'react-icons/fi';
+import logo from '../../../assets/image/CapiPlay.png'
+import { AiOutlineSearch } from 'react-icons/ai'; 
 import './Header.css'
 import { Button } from 'bootstrap';
 
@@ -8,15 +10,16 @@ function Header() {
   return (
     <div className='container__header'>
       <div className='box__header'>
-        <GiHamburgerMenu className='menu__icon' color='var(--lightpurple)' fontSize={35}>
+        <GiHamburgerMenu className='menu__icon' color='var(--lightpurple)' fontSize={30}>
           <Button></Button>
         </GiHamburgerMenu>
-        <div className='container__logo'></div>
+        <div className='container__logo'>
+        <img src={logo} className='container__perfilImage'/>
+        </div>
       </div>
       <div className='box__header'>
-        <AiOutlineSearch className='menu__icon' color='var(--lightpurple)' fontSize={35}>
-          <Button></Button>
-        </AiOutlineSearch>
+        <AiOutlineSearch className='menu__icon' color='var(--lightpurple)' fontSize={25}/>
+        <FiUpload className='menu__icon' color='var(--lightpurple)' fontSize={25}/>
         <img src="https://inte.upc.edu/en/shared/img/pingu.jpeg/@@images/898e6d56-4779-44f8-904b-8c1878a7a264.jpeg" className='container__perfilImage'/>
       </div>
     </div>
