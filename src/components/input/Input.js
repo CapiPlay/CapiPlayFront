3b51064b812c02062830a7a57d8b33961dce2be4
GabrielRID <gabrielrricardodavela@gmail.com>
2023-07-06 21:43:46 -0300

    Página de login finalizada

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,33 +1,61 @@
-
+import React, { useRef, useEffect, useState } from "react"
 import "./Input.css"
 
-
-// placeholder (string): O texto exibido como dica dentro do campo de entrada.
-// value (string): O valor atual do campo de entrada.
-// onChange (function): Uma função chamada quando o valor do campo de entrada é alterado.
-// onClick (function): Uma função chamada quando o campo de entrada é clicado.
-// required (boolean): Indica se o campo de entrada é obrigatório ou não.
-// type (string): O tipo de campo de entrada, como "text", "password", "email", entre outros.
-// enable (boolean): Indica se o campo de entrada está habilitado ou desabilitado.
-
 const Input = ({ placeholder, value, onChange, onClick, required, type, enable }) => {
-
-    const hasValue = value && value.length > 0
     
-    return (
-        <div className="input__group">
-            <input
-                className="input__control"
-                type={type}
-                value={value}
-                onChange={onChange}
-                onClick={onClick}
-                enable={enable}
-                required={required}
-            />
-            <label className={`label__input ${hasValue ? "active" : ""}`}>{placeholder}</label>
-        </div>
-    )
+  const [labelColor, setLabelColor] = useState({})
+  const [inputBorderColor, setInputBorderColor] = useState({})
+
+  const ref = useRef(null)
+  const hasValue = value && value.length > 0
+
+  useEffect(() => {
+    const handleFocus = () => {
+      setLabelColor({ color: "#BF94FF" })
+      setInputBorderColor({ borderColor: "#BF94FF" })
+    }
+
+    const handleBlur = () => {
+      if (!hasValue) {
+        setLabelColor({ color: "#141317" })
+        setInputBorderColor({ borderColor: "#141317" })
+      }
+    }
+
+    const inputElement = ref.current
+
+    if (inputElement) {
+      inputElement.addEventListener("focus", handleFocus)
+      inputElement.addEventListener("blur", handleBlur)
+    }
+
+    return () => {
+      if (inputElement) {
+        inputElement.removeEventListener("focus", handleFocus)
+        inputElement.removeEventListener("blur", handleBlur)
+      }
+    }
+  }, [hasValue])
+
+  return (
+    <div className="input__group">
+      <input
+        className="input__control"
+        type={type}
+        value={value}
+        onChange={onChange}
+        onClick={onClick}
+        enable={enable}
+        required={required}
+        ref={ref}
+        style={inputBorderColor}
+        name={type}
+      />
+      <label className={`label__input ${hasValue ? "active" : ""}`} style={labelColor}>
+        {placeholder}
+      </label>
+    </div>
+  )
 }
 
 export default Input
