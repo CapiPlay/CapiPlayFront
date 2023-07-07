6b92bd874d8cba451d7e9dc6f47414de0a50258c
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-07-06 21:45:07 -0300

    att

--- src/pages/player/Player.js
+++ src/pages/player/Player.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Like from './like_btn/Like_btn'
 import Dislike from './dislike_btn/Dislike_btn'
 import { AiFillEye, AiFillHeart } from 'react-icons/ai'
@@ -11,53 +11,150 @@ import Comments_component from './comments_componet/Comments_component'
 import Video_card from '../../components/video_card/Video_card'
 
 function Player() {
+
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const verifyScreen = () => {
+    if (screenSize.width > 900) {
+      return false
+    } else {
+      return true
+    }
+  }
+
   return (
     <>
-      <div>
-        <video controls className='video__player'>
-          <source src={pingu} type="video/mp4" />
-        </video>
-      </div>
-      <div className='video__title'>
-        <p>Pingu.</p>
-      </div>
-      <div className='interaction'>
-        <div className='interaction__info'>
-          <div className='views__div'>
-            <AiFillEye size={'1.3rem'} /> 57k de Visualizações
-          </div>
-          <div className='likes__div'>
-            <AiFillHeart size={'1.25rem'} /> 57k de Likes
-          </div>
-        </div>
-        <div className='like__dislike__btns'>
-          <Like />
-          <Dislike />
-        </div>
-      </div>
-      <div>
-        <Channel_component />
-      </div>
-      <div>
-        <Description_component/>
-      </div>
-      <div>
-        <Divider_component/>
-      </div>
-      <div className='comments__container'>
-        <div className='total__comments'>
-          <p>Comentários</p>
-        </div>
+      {verifyScreen() ?
         <div>
-          <Comments_component/>
+          <div>
+            <video controls className='video__player'>
+              <source src={pingu} type="video/mp4" />
+            </video>
+          </div>
+          <div className='video__title'>
+            <p>Pingu.</p>
+          </div>
+          <div className='interaction'>
+            <div className='interaction__info'>
+              <div className='views__div'>
+                <AiFillEye size={'1.3rem'} /> 57k de Visualizações
+              </div>
+              <div className='likes__div'>
+                <AiFillHeart size={'1.25rem'} /> 57k de Likes
+              </div>
+            </div>
+            <div className='like__dislike__btns'>
+              <Like />
+              <Dislike />
+            </div>
+          </div>
+          <div>
+            <Channel_component />
+          </div>
+          <div>
+            <Description_component />
+          </div>
+          <div>
+            <Divider_component />
+          </div>
+          <div className='comments__container'>
+            <div className='total__comments'>
+              <p>Comentários</p>
+            </div>
+            <div>
+              <Comments_component />
+            </div>
+          </div>
+          <div>
+            <Divider_component />
+          </div>
+          <div>
+            <div className='video__card'>
+              <Video_card />
+            </div>
+            <div className='video__card'>
+              <Video_card />
+            </div>
+          </div>
+        </div> 
+        
+        :
+
+        <div className='things'>
+          <div>
+            <div>
+              <video controls className='video__player'>
+                <source src={pingu} type="video/mp4" />
+              </video>
+            </div>
+            <div className='video__title'>
+              <p>Pingu.</p>
+            </div>
+            <div className='interaction'>
+              <div className='interaction__info'>
+                <div className='views__div'>
+                  <AiFillEye size={'1.3rem'} /> 57k de Visualizações
+                </div>
+                <div className='likes__div'>
+                  <AiFillHeart size={'1.25rem'} /> 57k de Likes
+                </div>
+              </div>
+              <div className='like__dislike__btns'>
+                <Like />
+                <Dislike />
+              </div>
+            </div>
+            <div>
+              <Channel_component />
+            </div>
+            <div>
+              <Description_component />
+            </div>
+            <div>
+              <Divider_component />
+            </div>
+            <div className='comments__container'>
+              <div className='total__comments'>
+                <p>Comentários</p>
+              </div>
+              <div>
+                <Comments_component />
+              </div>
+            </div>
+            <div>
+              <Divider_component />
+            </div>
+          </div>
+          <div>
+            <div className='video__card'>
+              <Video_card />
+            </div>
+            <div className='video__card'>
+              <Video_card />
+            </div>
+            <div className='video__card'>
+              <Video_card />
+            </div>
+            <div className='video__card'>
+              <Video_card />
+            </div>
+            <div className='video__card'>
+              <Video_card />
+            </div>
+          </div>
         </div>
-      </div>
-      <div>
-        <Divider_component/>
-      </div>
-      <div>
-        <Video_card/>
-      </div>
+      }
     </>
   )
 }
--- src/pages/player/channel_component/Channel_component.js
+++ src/pages/player/channel_component/Channel_component.js
@@ -5,12 +5,14 @@ function Channel_component() {
   return (
     <div>
         <div className='component'>
-            <div className='channel'>
-                <img src='https://yt3.ggpht.com/PFRD_rpPwAIY-FC2t6Ob0GpJe2udeEaXNwug4Dx8v7zxxda6ZKHU1aKBX-XoWvYh2H4Ow6TtBDk=s176-c-k-c0x00ffffff-no-rj-mo' className='channel__image'/>
-            </div>
-            <div className='channel__info'>
-                <div className='channel__name'>Pingu - OfficialChannel</div>
-                <div className='channel__subs'>50 mi de inscritos</div>
+            <div className='channel__container'>
+                <div className='channel'>
+                    <img src='https://yt3.ggpht.com/PFRD_rpPwAIY-FC2t6Ob0GpJe2udeEaXNwug4Dx8v7zxxda6ZKHU1aKBX-XoWvYh2H4Ow6TtBDk=s176-c-k-c0x00ffffff-no-rj-mo' className='channel__image'/>
+                </div>
+                <div className='channel__info'>
+                    <div className='channel__name'>Pingu - OfficialChannel</div>
+                    <div className='channel__subs'>50 mi de inscritos</div>
+                </div>
             </div>
             <div className='subscribe'>
                 <button className='subscribe__btn'>inscrever-se</button>
