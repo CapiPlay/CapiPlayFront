d51e021de68cf81c8f9ef311086f02239f75b43d
Gustavo Guilherme de Abreu <137020767+GustavoAbreu01@users.noreply.github.com>
2023-07-06 21:48:18 -0300

    ATT

--- src/pages/home/header/Header.js
+++ src/pages/home/header/Header.js
@@ -2,25 +2,31 @@ import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { FiUpload } from 'react-icons/fi';
 import logo from '../../../assets/image/CapiPlay.png'
-import { AiOutlineSearch } from 'react-icons/ai'; 
+import { AiOutlineSearch } from 'react-icons/ai';
 import './Header.css'
-import { Button } from 'bootstrap';
+import Modal_menu from '../modal_menu/Modal_menu';
+import Aos from 'aos';
 
 function Header() {
+
+  Aos.init({
+    duration: 200
+  });
+
   return (
     <div className='container__header'>
       <div className='box__header'>
-        <GiHamburgerMenu className='menu__icon' color='var(--lightpurple)' fontSize={30}>
-          <Button></Button>
-        </GiHamburgerMenu>
+        <div className='modal__menu'>
+          <Modal_menu />
+        </div>
         <div className='container__logo'>
-        <img src={logo} className='container__perfilImage'/>
+          <img src={logo} className='container__perfilImage' />
         </div>
       </div>
       <div className='box__header'>
-        <AiOutlineSearch className='menu__icon' color='var(--lightpurple)' fontSize={25}/>
-        <FiUpload className='menu__icon' color='var(--lightpurple)' fontSize={25}/>
-        <img src="https://inte.upc.edu/en/shared/img/pingu.jpeg/@@images/898e6d56-4779-44f8-904b-8c1878a7a264.jpeg" className='container__perfilImage'/>
+        <AiOutlineSearch className='menu__icon' color='var(--lightpurple)' fontSize={25} />
+        <FiUpload className='menu__icon' color='var(--lightpurple)' fontSize={25} />
+        <img src="https://inte.upc.edu/en/shared/img/pingu.jpeg/@@images/898e6d56-4779-44f8-904b-8c1878a7a264.jpeg" className='container__perfilImage' />
       </div>
     </div>
   )
--- src/pages/home/modal_menu/modal_menu.js
+++ src/pages/home/modal_menu/modal_menu.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import './Modal_menu.css'
+import Aos from 'aos';
 
-function modal_menu() {
-  return (
-    <div>
+function Modal_menu() {
+    const [openModal, setOpenModal] = useState(0)
 
-    </div>
-  )
+    function verify() {
+        if (openModal !== 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <GiHamburgerMenu className="MenuIcon" onClick={() => setOpenModal(openModal + 1)}></GiHamburgerMenu>
+            </div>
+            {verify() &&
+                <div>
+                    <div data-aos='fade-right' className="modal-menu-content">
+                        <ul className="modal-menu-list">
+                            <li className="modal-menu-item">Início</li>
+                            <li className="modal-menu-item">Em alta</li>
+                            <li className="modal-menu-item">Inscrições</li>
+                            <li className="modal-menu-item">Originais</li>
+                            <li className="modal-menu-item">Histórico</li>
+                        </ul>
+                    </div>
+                </div>
+            }
+            {verify() &&
+                <div className="background" onClick={() => setOpenModal(openModal - 1)}></div>
+            }
+        </>
+    )
 }
 
-export default modal_menu
+export default Modal_menu
