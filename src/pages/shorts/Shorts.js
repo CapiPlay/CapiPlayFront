7549624c937ecd1b5b2ad71003e5edc9c622ec24
AlineDeSouza30 <desouzaaline2004@gmail.com>
2023-07-07 21:41:40 -0300

    Iniciando página de shorts

--- src/pages/shorts/Shorts.js
+++ src/pages/shorts/Shorts.js
@@ -1,6 +1,61 @@
+import { useEffect, useState } from 'react'
+
+import '../shorts/Shorts.css'
+
+//imagens
+import imageShorts from "../../assets/imagemShorts.png"
+import imagePerfil from "../../assets/imagemPerfil.png"
+
+//componentes
+import Button from '../../components/button/Button.js'
+
+//icons
+import { BiLike } from "react-icons/bi"
+import { BiDislike } from "react-icons/bi"
+import { BiCommentDetail } from "react-icons/bi"
+
 const Shorts = () => {
 
-    
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowHeight(window.innerHeight)
+        }
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+    return (
+        <div className='container__all__shorts' style={{ minHeight: `${windowHeight}px` }} >
+
+            {/* HEADER e BACK HEADER*/}
+            
+            <div className='container__video'>
+                <img src={imageShorts} alt="Imagem shorts" />
+
+                <div className='container__icons__shorts'>
+                    <BiLike />
+                    <span>32K</span>
+                    <BiDislike />
+                    <BiCommentDetail />
+                </div>
+
+                <div className='container__informations__video'>
+                    <span>Como montar rapidamente um cubo mágico muito rápido</span>
+                    <img src={imagePerfil} alt="Imagem de Perfil" />
+                    <span>Chill Vibes</span>
+                    <Button label={"Entrar"} onClick={null} type={"submit"} />
+                </div>
+
+            </div>
+
+        </div>
+    )
 
 }
 export default Shorts
