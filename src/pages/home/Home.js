7bd8214c8a6a73e01ef74b7c0e5d78cb6ee2224d
GustavoAbreu01 <137020767+GustavoAbreu01@users.noreply.github.com>
2023-07-07 21:47:46 -0300

    ATT

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './header/Header'
 import './Home.css'
 import MySlider from './slider/Slider'
 import Video_card from '../../components/video_card/Video_card'
 
 function Home() {
+
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const verifyScreen = () => {
+    if (screenSize.width > 900) {
+      return false
+    } else {
+      return true
+    }
+  }
+
   return (
     <><Header />
       <div className='container__home'>
