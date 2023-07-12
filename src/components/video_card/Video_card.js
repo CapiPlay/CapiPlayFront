6f490ef698802a55f1d4f21a3c323fe4bb5cc711
erikawk <105462871+erikawk@users.noreply.github.com>
2023-07-11 21:42:34 -0300

    fazendo lógica do histórico e criando objeto de video no VideoCard

--- src/components/video_card/Video_card.js
+++ src/components/video_card/Video_card.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import './Video_card.css'
 
-function Video_card() {
+// const video = {
+//     uuid,
+//     caminho,
+//     descricao,
+//     titulo,
+//     tags,
+//     categoria
+// }
+
+function Video_card({video}) {
     return (
         <div className='box__video__card'>
             <div className='container__video__image'>
-                <img src="https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg" className='container__video__card__image' />
+                <img src={video.caminho} className='container__video__card__image' />
             </div>
             <div className='container__video__info'>
                 <img src="https://1.bp.blogspot.com/_i5HYEqTAi9w/SfkWb4gS0jI/AAAAAAAABXE/8BEdz7gYctA/s280/Pingu1.jpg" className='container__video__perfilImage' />
                 <div className='container__video__info__text'>
-                    <h5 className='container__video__title__text'>Morning Vibes 🍀 Positive Feelings </h5>
+                    <h5 className='container__video__title__text'>{video.titulo}</h5>
                     <div>
                         <h5 className='container__video__perfilName'>Nome do Canal</h5>
                         <h6 className='container__video__views'>10K de visualizações  ---  57K de Likes</h6>
--- src/pages/historic/Historic.js
+++ src/pages/historic/Historic.js
@@ -1,6 +1,76 @@
+import "./Historic.css"
+import { useState } from "react"
+import { format } from 'date-fns';
+import HeaderToBack from "../../components/headerToBack/HeaderToBack"
+import Video_card from "../../components/video_card/Video_card"
+
 const Historic = () => {
 
-    
+    const [shorts, setShorts] = useState([]);
+    const [videos, setVideos] = useState([
+        {
+            uuid: 1,
+            caminho: "",
+            descricao: "descricao video 1",
+            titulo: "titulo video 1",
+            tags: [{}],
+            categoria: {},
+            date: new Date('2023-07-10')
+        },
+        {
+            uuid: 2,
+            caminho: "",
+            descricao: "descricao video 2",
+            titulo: "titulo video 2",
+            tags: [{}],
+            categoria: {},
+            date: new Date('2023-07-09')
+        }
+    ]);
 
+    const dateTest = new Date("2023-05-18")
+    const dateFormat = 'dd/MM/yy';
+
+    const addToHistoric = (video) => {
+        const videoDate = { ...video, date: new Date() };
+        setVideos((prevVideos) => [videoDate, ...prevVideos])
+    }
+
+    const videotest = {
+        caminho: "https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg"
+    }
+
+    return (
+        <div className="historic__container">
+            <HeaderToBack text={"Histórico"} route={"/"} />
+            {
+                videos || shorts
+                    ?
+                    <div className="historic__date">
+                        <span>{format(dateTest, dateFormat)}</span>
+                        <Video_card video={videotest}/>
+                        {shorts != null &&
+                            <div className="container__shorts__historic">
+                                <div></div>
+                                <div></div>
+                            </div>
+                        }
+                        {videos &&
+                            <div className="container__videos__historic">
+                                {videos.map((video) => {
+                                    <>
+                                        <h3>{video.title}</h3>
+                                        <span>{video.descricao}</span>
+                                        <span>{format(video.date, dateFormat)}</span>
+                                    </>
+                                })}
+                            </div>
+                        }
+                    </div>
+                    :
+                    <span>Você ainda não viu nenhum vídeo</span>
+            }
+        </div>
+    )
 }
 export default Historic
