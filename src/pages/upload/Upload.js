4c41b7084a556881dfed323b802a12fbf828573b
Bruneb4s <105462955+Brunebas@users.noreply.github.com>
2023-07-20 19:20:01 -0300

    ATT

--- src/pages/upload/Upload.js
+++ src/pages/upload/Upload.js
@@ -1,86 +1,43 @@
 import './Upload.css'
 
 import { useState } from "react"
-
-import Input from "../../components/input/Input";
-import InputFile from "../../components/inputFile/InputFile";
+import Button from '../../components/button/Button';
 
 function Upload() {
 
-    const objVideo = {
-        titulo: '',
-        descricao: '',
-        categoria: '',
-        thumbnail: '',
-        tags: '',
-        kids: ''
+    const [isVideo, setIsVideo] = useState(true)
+
+    const handleVideoChange = () => {
+        setIsVideo(true)
     }
-
-    const [videoData, setVideoData] = useState(objVideo)
-
-    const handleInputChange = (e) => {
-        setVideoData({ ...videoData, [e.target.name]: e.target.value })
+    const handleShortsChange = () => {
+        setIsVideo(false)
     }
 
     return (
         <>
-        <div className='upload__'>
-            <Input
-                placeholder={"Título do vídeo"}
-                type={"text"}
-                onChange={handleInputChange}
-                required={true}
-            />
+            <div>
 
-            <Input
-                placeholder={"Descrição do vídeo"}
-                type={"text"}
-                onChange={handleInputChange}
-                required={true}
-            />
+                <div>
+                    <Button
+                        label={"Vídeo"}
+                        onClick={handleVideoChange}
+                        type={"button"}
+                    />
+                    <Button
+                        label={"CapiShorts"}
+                        onClick={handleShortsChange}
+                        type={"button"}
+                    />
+                </div>
+            </div>
 
-            <select>
-                <option>Artes e Cultura</option>
-                <option>Ciência e Tecnologia</option>
-                <option>Culinária</option>
-                <option>Educação</option>
-                <option>Entretenimento</option>
-                <option>Esportes</option>
-                <option>Documentários</option>
-                <option>Jogos</option>
-                <option>Lifestyle</option>
-                <option>Moda e Beleza</option>
-                <option>Música</option>
-                <option>Viagem e Turismo</option>
-            </select>
-        </div>
-
-            <InputFile
-                label={"Foto de perfil"}
-                radius={"20px"}
-                onChange={handleInputChange}
-                // file={image}
-            />
-
-            <Input
-                placeholder={"Descrição do vídeo"}
-                type={"text"}
-                onChange={handleInputChange}
-                required={true}
-            />
-
-            <Input
-                placeholder={"Descrição do vídeo"}
-                type={"radio"}
-                onChange={handleInputChange}
-                required={true}
-            />
-            <Input
-                placeholder={"Descrição do vídeo"}
-                type={"radio"}
-                onChange={handleInputChange}
-                required={true}
-            />
+            {isVideo &&
+                <div>videoooooooooooooooooo</div>
+            }
+            {!isVideo &&
+                <div>shooooooooooooooooorts</div>
+            }
         </>
     )
 }
