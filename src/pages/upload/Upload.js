4dd1e96dc8c3fb820422e047f56764ebccdbff32
Bruneb4s <105462955+Brunebas@users.noreply.github.com>
2023-07-20 20:58:38 -0300

    att

--- src/pages/upload/Upload.js
+++ src/pages/upload/Upload.js
@@ -5,16 +5,27 @@ import HeaderUpload from './headerUpload/HeaderUpload';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 import Button from "../../components/button/Button";
+import InputFile from '../../components/inputFile/InputFile';
 
 function Upload() {
 
     const [isVideo, setIsVideo] = useState(true)
+    const [image, setImage] = useState()
 
     const handleVideoChange = () => {
         setIsVideo(true)
     }
     const handleShortsChange = () => {
         setIsVideo(false)
+    }
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0]
+        if (file) {
+            const formData = new FormData()
+            formData.append("foto", file)
+            setImage(formData)
+        }
     }
 
     return (
@@ -46,10 +57,23 @@ function Upload() {
                             </>
                         }
                     </div>
+                    <div className='upload__divider' />
                     <div>
                         {isVideo &&
                             <>
+                                <InputFile
+                                    label={"Foto de perfil"}
+                                    radius={"20px"}
+                                    onChange={handleFileChange}
+                                    file={image}
+                                />
                                 <div>videoooooooooooooooooo</div>
+                                <Button
+                                    label={"Entrar"}
+                                    // onClick={}
+                                    type={"submit"}
+                                    principal={true}
+                                />
                                 <Button
                                     label={"Entrar"}
                                     // onClick={}
@@ -60,13 +84,23 @@ function Upload() {
                         }
                         {!isVideo &&
                             <>
-                                <div>shooooooooooooooooorts</div>
-                                <Button
-                                    label={"Entrar"}
-                                    // onClick={}
-                                    type={"submit"}
-                                    principal={true}
-                                />
+                                <div>
+                                    <div>shooooooooooooooooorts</div>
+                                    <div className='upload__next__buttons__box'>
+                                        <Button
+                                            label={"Entrar"}
+                                            // onClick={}
+                                            type={"submit"}
+                                            principal={true}
+                                        />
+                                        <Button
+                                            label={"Entrar"}
+                                            // onClick={}
+                                            type={"submit"}
+                                            principal={true}
+                                        />
+                                    </div>
+                                </div>
                             </>
                         }
                     </div>
