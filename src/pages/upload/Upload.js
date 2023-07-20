95dd1d731abd0f7aee6bd901944927d592bdf3ba
Bruneb4s <105462955+Brunebas@users.noreply.github.com>
2023-07-20 20:21:53 -0300

    ATT

--- src/pages/upload/Upload.js
+++ src/pages/upload/Upload.js
@@ -1,7 +1,10 @@
 import './Upload.css'
 
 import { useState } from "react"
-import Button from '../../components/button/Button';
+import HeaderUpload from './headerUpload/HeaderUpload';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+
+import Button from "../../components/button/Button";
 
 function Upload() {
 
@@ -16,28 +19,60 @@ function Upload() {
 
     return (
         <>
-            <div>
-
-                <div>
-                    <Button
-                        label={"Vídeo"}
-                        onClick={handleVideoChange}
-                        type={"button"}
-                    />
-                    <Button
-                        label={"CapiShorts"}
-                        onClick={handleShortsChange}
-                        type={"button"}
-                    />
+            <div className='upload__page'>
+                <HeaderUpload />
+                <div className='upload__container'>
+                    <div className='upload__buttons_box'>
+                        {isVideo &&
+                            <>
+                                <button className='upload_button_selected' type='button' onClick={handleVideoChange}>
+                                    <AiOutlineArrowLeft className='arrow__icon' color='var(--lightpurple)' fontSize={25} />
+                                    Vídeo
+                                </button>
+                                <button className='upload_button' type='button' onClick={handleShortsChange}>
+                                    <AiOutlineArrowLeft className='arrow__icon' color='var(--lightpurple)' fontSize={25} />                            CapiShorts
+                                </button>
+                            </>
+                        }
+                        {!isVideo &&
+                            <>
+                                <button className='upload_button' type='button' onClick={handleVideoChange}>
+                                    <AiOutlineArrowLeft className='arrow__icon' color='var(--lightpurple)' fontSize={25} />
+                                    Vídeo
+                                </button>
+                                <button className='upload_button_selected' type='button' onClick={handleShortsChange}>
+                                    <AiOutlineArrowLeft className='arrow__icon' color='var(--lightpurple)' fontSize={25} />                            CapiShorts
+                                </button>
+                            </>
+                        }
+                    </div>
+                    <div>
+                        {isVideo &&
+                            <>
+                                <div>videoooooooooooooooooo</div>
+                                <Button
+                                    label={"Entrar"}
+                                    // onClick={}
+                                    type={"submit"}
+                                    principal={true}
+                                />
+                            </>
+                        }
+                        {!isVideo &&
+                            <>
+                                <div>shooooooooooooooooorts</div>
+                                <Button
+                                    label={"Entrar"}
+                                    // onClick={}
+                                    type={"submit"}
+                                    principal={true}
+                                />
+                            </>
+                        }
+                    </div>
                 </div>
             </div>
 
-            {isVideo &&
-                <div>videoooooooooooooooooo</div>
-            }
-            {!isVideo &&
-                <div>shooooooooooooooooorts</div>
-            }
         </>
     )
 }
