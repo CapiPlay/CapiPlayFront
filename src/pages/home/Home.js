aff63483d9d5e848c3018b959e513b17e4c461c3
Gustavo Guilherme de Abreu <137020767+GustavoAbreu01@users.noreply.github.com>
2023-07-20 21:35:57 -0300

    ATT

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,7 +6,6 @@ import Slider from './slider/Slider';
 import Video_card from '../../components/video_card/Video_card';
 import Side_Bar from './side_bar/Side_Bar';
 import Slider_Category from './slider_category/Slider_Category';
-import Header_Tablet from './header_tablet/Header_Tablet';
 import Slider_Shorts from '../../components/slider_shorts/Slider_Shorts';
 
 function Home() {
