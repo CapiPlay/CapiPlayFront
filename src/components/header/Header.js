437e8c0a0e4a886e7d3cbb56aed67d8958bcb201
AlineDeSouza30 <desouzaaline2004@gmail.com>
2023-07-27 19:18:11 -0300

    Fazendo para trocar de video

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -20,7 +20,7 @@ function Header() {
                     <TbUpload />
                 </div>
                 <div>
-                    <img src='https://yt3.ggpht.com/PFRD_rpPwAIY-FC2t6Ob0GpJe2udeEaXNwug4Dx8v7zxxda6ZKHU1aKBX-XoWvYh2H4Ow6TtBDk=s176-c-k-c0x00ffffff-no-rj-mo' />
+                    <img src='https://yt3.ggpht.com/PFRD_rpPwAIY-FC2t6Ob0GpJe2udeEaXNwug4Dx8v7zxxda6ZKHU1aKBX-XoWvYh2H4Ow6TtBDk=s176-c-k-c0x00ffffff-no-rj-mo' alt='Imagem de Perfil' />
                 </div>
             </div>
         </div>
