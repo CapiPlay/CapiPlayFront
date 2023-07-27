c8eaba85b07e2206816df93acad2772c7c081288
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-07-27 19:00:36 -0300

    so pra nao perder nada :)

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -6,12 +6,25 @@ import HeaderProfile from '../profile/header_profile/HeaderProfile'
 import Side_Bar from '../home/side_bar/Side_Bar'
 import Video_card from '../../components/video_card/Video_card'
 import Header from '../../components/header/Header'
+import UserService from '../../service/UserService'; // Importe o serviço de produtos aqui
+
 
 import ProfilePicture from '../../assets/image/channel_profile.png'
 
-function Profile() {
+
+
+const Profile = ({ user }) => {
 
     const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+      // Quando o componente for montado, buscar os produtos e atualizar o estado
+      UserService.findAll()
+        .then((data) => setUsers(data))
+        .catch((error) => console.error('Erro ao buscar usuario:', error));
+    }, []);
 
     useEffect(() => {
         function handleResize() {
@@ -37,28 +50,34 @@ function Profile() {
             {verifyScreen() ?
                 <>
                     <HeaderProfile />
-                    <div className='profile__container'>
-                        <div className='profile__container__picture'>
-                            <img className="profile__pic" src={ProfilePicture} />
-                            <h2 className='profile__name'>Thomas Turbando</h2>
-                            <button className='profile__subscribe__button'>Inscrever-se</button>
-                            <p className='profile__id'>@Paula_Tejanto</p>
-                            <div className='profile__details'>
-                                <p>45k Inscritos</p>
-                                <p>13 Vídeos</p>
+                    <div>
+                        {users.map((user) => (
+                            <div key={user.id}  >
+                                <div className='profile__container'>
+                                    <div className='profile__container__picture' key={user.id}>
+                                        <img className="profile__pic" src={ProfilePicture} />
+                                        <h2 className='profile__name'> {user.name}</h2>
+                                        <button className='profile__subscribe__button'>Inscrever-se</button>
+                                        <p className='profile__id'> {user.id}</p>
+                                        <div className='profile__details'>
+                                            <p> {user.subscribers}</p>
+                                            <p> {user.videos}</p>
+                                        </div>
+                                        <div className='profile__description'>
+                                            <p> {user.description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr class="solid" />
+                                <div className='profile__box__videos'>
+                                    <Video_card />
+                                    <Video_card />
+                                    <Video_card />
+                                    <Video_card />
+                                    <Video_card />
+                                </div>
                             </div>
-                            <div className='profile__description'>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sapien nisi, dictum sit amet lacinia sed, gravida sed urna. Duis a scelerisque purus. Suspendisse blandit hendrerit diam, nec eleifend eros tempus non. Mauris consectetur orci sed nisl suscipit, congue consectetur nulla dapibus. Maecenas vel orci dictum nibh fringilla fermentum vitae et sapien. Duis id arcu tempus.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <hr class="solid" />
-                    <div className='profile__box__videos'>
-                        <Video_card />
-                        <Video_card />
-                        <Video_card />
-                        <Video_card />
-                        <Video_card />
+                        ))}
                     </div>
                 </>
                 :
---
+++ src/service/UserService.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const url = 'http://localhost:8081/user'
+
+export const UserService = {
+
+  create: function (user) {
+    console.log(user)
+    axios.post(url, user).then((response) => {
+      return response.data;
+    });
+  },
+
+  findAll: function () {
+    return new Promise((resolve, reject) => {
+      axios.get(url)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
+
+  findOne: function (id) {
+    axios.get(url + '/' + id).then((response) => {
+      return response.data;
+    });
+  },
+  edit: function (id, user) {
+    axios.patch(url + '/' + id, user).then((response) => {
+      return response.data
+    })
+  },
+  delete: function(id){
+    axios.delete(url + '/'+ id).then((response) => {
+        return response.data;
+      });
+}
+
+}
+export default UserService;
