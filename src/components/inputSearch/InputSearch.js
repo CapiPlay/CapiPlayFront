f59d902b11ed79cda45a4a88f98076a6ab1388f3
erikawk <105462871+erikawk@users.noreply.github.com>
2023-07-27 21:44:14 -0300

    finalizando a página de pesquisa
    precisa ser feito ainda a sobreposição da pesquisa na home

--- src/components/inputSearch/InputSearch.js
+++ src/components/inputSearch/InputSearch.js
@@ -1,11 +1,24 @@
 import { BiSearchAlt2 } from "react-icons/bi"
 import "./InputSearch.css"
-const InputSearch = () => {
+import { useNavigate } from "react-router-dom"
+
+const InputSearch = ({ value, handleChange}) => {
+
+    // const [researched, setResearched] = useState(false); 
+
+    // const handleClick = () => {
+    //     setResearched(true);
+    // }
 
     return (
         <div className="container__input__search">
-            <input type="text" />
-            <BiSearchAlt2 color="var(--lightpurple)" size={20} />
+            <input
+                type="text"
+                value={value}
+                onChange={handleChange} />
+            <BiSearchAlt2
+                className="icon__search"
+                 />
         </div>
     )
 }
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 // ícones
 import { IoMdArrowBack } from "react-icons/io"
 import { MdRestartAlt } from "react-icons/md"
+import { BiSearchAlt2 } from "react-icons/bi"
 
 // componentes
 import InputSearch from "../../components/inputSearch/InputSearch";
@@ -12,31 +13,73 @@ import { useState } from "react";
 const Search = () => {
 
     const nav = useNavigate();
+    const sizeIcon = 20;
 
-    // state exemplar
-    const [lastSearches, setLastSearches] = useState
-        ([
-            "Filme como treinar seu dragão é bom?",
-            "Pica - Pau completo dublado",
-            "Como fazer uma torta de abacaxi com calda de côco?"
-        ]);
+    const [lastSearches, setLastSearches] = useState(([
+        "Benefícios da meditação para a saúde",
+        "Receita de bolo de cenoura com cobertura de chocolate",
+        "Principais destinos turísticos na Europa",
+        "História da America Latina",
+        "Receita de pão de queijo",
+        "Livros românticos",
+        "Eu a patroa e as criancas",
+        "React icons como funciona",
+        "Torta de frango receita",
+        "Livros de aventura 2023",
+
+    ]));
+
+    const [searches, setSearches] = useState(([
+        "Filme como treinar seu dragão é bom?",
+        "Pica - Pau completo dublado",
+        "Como fazer uma torta de abacaxi com calda de côco?"
+    ]));
+
+    const [valueInput, setValueInput] = useState("");
 
     const handleClick = () => {
         nav("/");
     }
 
+    const handleChange = (e) => {
+        setValueInput(e.target.value);
+    }
+
+    const renderSearch = () => {
+        if (valueInput.trim() === '') {
+            return (
+                <>
+                    {lastSearches && lastSearches.map((lastSearch) => (
+                        <div className="search__box">
+                            <MdRestartAlt size={sizeIcon} color="var(--whitesmoke)" />
+                            <span>{lastSearch}</span>
+                        </div>
+                    ))}
+                </>
+            )
+        } else {
+            return (
+                <>
+                    {searches && searches.map((search) => (
+                        <div className="search__box">
+                            <BiSearchAlt2 size={sizeIcon} color="var(--whitesmoke)" />
+                            <span>{search}</span>
+                        </div>
+                    ))}
+                </>
+            )
+        }
+    }
+
     return (
         <div className="container__search">
             <div className="header__search">
-                <IoMdArrowBack size={20} color="var(--lightpurple)" onClick={handleClick} />
-                <InputSearch />
+                <IoMdArrowBack size={sizeIcon} color="var(--lightpurple)" onClick={handleClick} />
+                <InputSearch
+                    value={valueInput}
+                    handleChange={handleChange} />
             </div>
-            {lastSearches && lastSearches.map((lastSearch) => (
-                <div className="container__last__search">
-                    <MdRestartAlt size={20} color="var(whitesmoke)" />
-                    <span>{lastSearch}</span>
-                </div>
-            ))}
+            {renderSearch()}
         </div>
     )
 }
