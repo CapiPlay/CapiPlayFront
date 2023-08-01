c698fb28a1535fb705f648bf3126c99db7e183c3
erikawk <105462871+erikawk@users.noreply.github.com>
2023-08-01 19:31:33 -0300

    fazendo o filtro

--- src/pages/resultSearch/ResultSearch.js
+++ src/pages/resultSearch/ResultSearch.js
@@ -1,10 +1,50 @@
 import "./ResultSearch.css"
 import TagsCarousel from "../../components/tagsCarousel/TagsCarousel";
+import { FiFilter } from "react-icons/fi"
+import { useState } from "react";
+const ResultSearch = () => {
 
-const ResultSearch = () =>{
-    return(
+    const [openFilter, setOpenFilter] = useState(false);
+
+    return (
         <div className="container__result__search">
-            <TagsCarousel/>
+            <div className="tags__carousel__search">
+                <TagsCarousel />
+            </div>
+            <div className="container__filter" onClick={() => setOpenFilter(!openFilter)}>
+                <span>Filtros</span>
+                <FiFilter />
+            </div>
+            {
+                openFilter &&
+                <>
+                    <table>
+                        <tr className="table__header__filter">
+                            <th>Data</th>
+                            <th>Duração</th>
+                            <th>Tipo</th>
+                        </tr>
+                        <td>
+                            <tr>Hoje</tr>
+                            <tr>Esta semana</tr>
+                            <tr>Este mês</tr>
+                            <tr>Este ano</tr>
+                        </td>
+                        <td>
+                            <tr>Hoje</tr>
+                            <tr>Esta semana</tr>
+                            <tr>Este mês</tr>
+                            <tr>Este ano</tr>
+                        </td>
+                        <td>
+                            <tr>Hoje</tr>
+                            <tr>Esta semana</tr>
+                            <tr>Este mês</tr>
+                            <tr>Este ano</tr>
+                        </td>
+                    </table>
+                </>
+            }
         </div>
     )
 }
