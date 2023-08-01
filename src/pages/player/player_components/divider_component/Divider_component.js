97ecf0d18560df5fbca5fb55e6e8fca67f9cfcd0
Gustavo Guilherme de Abreu <137020767+GustavoAbreu01@users.noreply.github.com>
2023-08-01 19:05:23 -0300

    ATT

---
+++ src/pages/player/player_components/divider_component/Divider_component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Divider_component.css'
+
+function Divider_component() {
+  return (
+    <div className='divider'></div>
+  )
+}
+
+export default Divider_component
