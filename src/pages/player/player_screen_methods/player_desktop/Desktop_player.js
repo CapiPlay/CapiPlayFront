efaf71b0c259459e3b0e351bcefe3ea5c2815f62
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-08-10 19:48:39 -0300

    att

--- src/pages/player/player_screen_methods/player_desktop/Desktop_player.js
+++ src/pages/player/player_screen_methods/player_desktop/Desktop_player.js
@@ -3,7 +3,6 @@ import './Desktop_player.css'
 import Like from '../../player_components/like_btn/Like_btn'
 import Dislike from '../../player_components/dislike_btn/Dislike_btn'
 import { AiFillEye, AiFillHeart } from 'react-icons/ai'
-import pingu from '../../../../assets/image/pingu.mp4'
 import Channel_component from '../../player_components/channel_component/Channel_component'
 import Description_component from '../../player_components/description_component/Description_component'
 import Divider_component from '../../player_components/divider_component/Divider_component'
@@ -12,12 +11,8 @@ import Video_card from '../../../../components/video_card/Video_card'
 import Header from '../../../../components/header/Header'
 import PlayerService from '../../../../service/PlayerService'
 
-//item (video) que vai ser o objeto vindo do back_end que conterá todas as informações
 function Desktop_player({ video }) {
     const [videos, setVideos] = useState([])
-    const [videoSource, setVideoSource] = useState(null);
-    // const videoReactions = EngajamentoService.buscarTodasReacoesPorVideo(video.uuid)
-    //são apenas variáveis de exemplo, elas vão vir com o objeto 
 
     useEffect(() => {
         getVideos()
@@ -27,15 +22,13 @@ function Desktop_player({ video }) {
         setVideos(await PlayerService.buscarVideosHome(0))
     }
 
-    const video_views_var = '57k'
-    const video_likes_var = '57k' //videoReactions.size
     return (
         <><Header></Header>
             <div className='space'></div>
             <div className='things'>
                 <div>
-                    <div>
-                        <video controls className='video__player__desktop' poster={"http://localhost:7000/api/video/static/" + video.caminhos[3]} autoPlay key={video.uuid}>
+                    <div className='video__container'>
+                        <video controls className='video__player__desktop' poster={"http://localhost:7000/api/video/static/" + video.caminhos[3]}  key={video.uuid}>
                             <source src={"http://localhost:7000/api/video/static/" + video.caminhos[5]} type="video/mp4" />
                         </video>
                     </div>
@@ -45,10 +38,10 @@ function Desktop_player({ video }) {
                     <div className='interaction'>
                         <div className='interaction__info'>
                             <div className='views__div'>
-                                <AiFillEye size={'1.3rem'} /> {video_views_var} de Visualizações
+                                <AiFillEye size={'1.3rem'} /> {video.curtidas} Visualizações
                             </div>
                             <div className='likes__div'>
-                                <AiFillHeart size={'1.25rem'} /> {video_likes_var} de Likes
+                                <AiFillHeart size={'1.25rem'} /> {video.visualizacoes} Likes
                             </div>
                         </div>
                         <div className='like__dislike__btns'>
@@ -88,7 +81,7 @@ function Desktop_player({ video }) {
                         </div>
                     </div>
                 </div>
-                <div className='videos'>
+                <div className='videos__desktop'>
                     {videos.map((video) => (
                         <Video_card key={video.uuid} video={video} />
                     ))}
