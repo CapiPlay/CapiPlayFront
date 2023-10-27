651ddd788ab0359a0ae0117857bbc76f80b3db8a
AlineDeSouza30 <desouzaaline2004@gmail.com>
2023-10-27 18:36:07 -0300

    .

--- src/components/commentsComponent/CommentsComponent.js
+++ src/components/commentsComponent/CommentsComponent.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import Comments from '../../pages/player/player_components/comments_componet/Comments_component'
 import '../commentsComponent/CommentsComponent.css'
 
@@ -6,6 +7,29 @@ import { IoMdClose, IoMdSend } from 'react-icons/io'
 
 const CommentsComponent = ({ func }) => {
 
+    // const [allComments, setAllComments] = useState([])
+
+    // const buscarComments = async () => {
+    //     var commentsTemp = await ComentarioService.buscarTodosPorVideo(video.uuid, page)
+    //     setPage(page + 1)
+    //     if(commentsTemp.content == null || commentsTemp.content == undefined){
+    //         setAllComments(null)
+    //     }else{
+    //         setLast(commentsTemp.last)
+    //         if(commentsTemp.content !== null || commentsTemp.content !== undefined){
+    //             setAllComments(commentsTemp.content)
+    //         }
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     buscarComments()
+    // }, [])
+
+    // useEffect(() => {
+    //     console.log(allComments)
+    // }, [allComments])
+
     return (
         <div className='container__comments__component'>
             <div>
@@ -13,10 +37,7 @@ const CommentsComponent = ({ func }) => {
                 <IoMdClose onClick={func} />
             </div>
             <div>
-                {/* <Comments />
-                <Comments />
-                <Comments />
-                <Comments /> */}
+                {/* <Comments /> */}
             </div>
             <div>
                 <input type="text" />
